8f5378af207641ee5f6c61c0e75058f6ebc7c35e
Gurpreet Singh <progurpreetsingh@gmail.com>
2022-04-08 09:53:30 +0530

    Change logo color

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     <header className="mx-4 mb-8">
       <h1 className="text-3xl text-green-600 text-center font-medium tracking-wide my-6">
         <Link to="/">
-          Mutual Funds <span className="text-orange-600">India</span>
+          Mutual Funds <span className="text-sky-600">India</span>
         </Link>
       </h1>
       <SearchForm />
